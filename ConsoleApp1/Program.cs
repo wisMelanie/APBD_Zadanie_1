0fb22e619d71e1086caadb0272fd33d00a2fb4da
wisMelanie <s25650@pjwstk.edu.pl>
2024-03-20 22:08:21 +0100

    Revert "Zadanie 3"
    This reverts commit 1cb39b5b3f5db9f2aa79d1be6efc898dbbfb4c40.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,22 +10,14 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int[] arr = { 1, 2, 3 };
-            double average = CalcAverage(arr);
-            Console.WriteLine(average);
-
-        }
-
-        static double CalcAverage(int[] arr)
-        {
-            double sum = 0;
-            for(int i = 0; i < arr.Length; i++)
-            {
-                sum += arr[i];
-            }
-
-            double average = sum / arr.Length;
-            return average;
+            // Modyfikacja 1
+            Console.WriteLine("Modyfikacja 1");
+            // Modyfikacja 2
+            int a = 2;
+            Console.WriteLine("Modyfikacja " + a);
+            // Modyfikacja 3
+            string b = "3";
+            Console.WriteLine("Modyfikacja " + b);
         }
     }
 }
